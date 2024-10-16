6488ab7d6f4c6a93b0592c98993bb03ffc6e6836
Gabriel Nadai <denadai123@gmail.com>
2024-10-16 18:10:49 -0300

    criacao novo comando converte horas semanais e seus testes

--- conversor_horas_trabalho/cli.py
+++ conversor_horas_trabalho/cli.py
@@ -3,6 +3,9 @@ from rich.table import Table
 from typer import Argument, Typer
 
 from conversor_horas_trabalho.converte_horas import converte_horas
+from conversor_horas_trabalho.converte_horas_semanais import (
+    converte_horas_semanais,
+)
 
 console = Console()
 app = Typer()
@@ -10,12 +13,41 @@ app = Typer()
 
 @app.command()
 def converte_hora(
-    hora: str=Argument('12:30', help='Hora em formato 12:30 ou 12.50')
+    hora: str = Argument('12:30', help='Hora em formato 12:30 ou 12.50')
 ):
     table = Table()
     hora_convertida = converte_horas(hora)
     for hr in hora_convertida:
         table.add_column(hr)
         table.add_row(hora_convertida['hora'][0])
+
+    console.print(table)
+
+
+@app.command()
+def converte_hora_semanal(
+    horas_semana: str = Argument('nm09.00,nm09.00', help='Tag mais Hora'),
+    formato: int = Argument(
+        '1', help='Formato de retorno com 1 para ":" ou 2 para "."'
+    ),
+):
+    table = Table()
+    lista_semana = []
+    lista_semana_split = horas_semana.split(',')
+    for hrstr in lista_semana_split:
+        prefix = hrstr[:2]
+        hr = hrstr[2:]
+        lista_semana.append([prefix, hr])
+
+    total_horas = converte_horas_semanais(lista_semana, formato)
+
+    table.add_column('total_horas_da_semana')
+    table.add_column('total_horas_extra')
+    table.add_column('total_horas_faltantes')
+    table.add_row(
+        total_horas['total_horas_da_semana'],
+        total_horas['total_horas_extra'],
+        total_horas['total_horas_faltantes'],
+    )
 
     console.print(table)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,23 +7,34 @@ runner = CliRunner()
 
 
 def test_converte_hora_cli_deve_retornar_zero_ao_stdout():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['converte-hora'])
     assert result.exit_code == 0
 
 
 def test_converte_hora_cli_deve_conter_as_horas_na_reposta():
     hora = '12.50'
-    result = runner.invoke(app)
+    result = runner.invoke(app, ['converte-hora'])
     assert hora in result.stdout
 
 
 @mark.parametrize('hora', ['12:30'])
 def test_converte_hora_cli_deve_conter_as_horas_com_um_ponto(hora):
-    result = runner.invoke(app, '12.50')
+    result = runner.invoke(app, ['converte-hora', '12.50'])
     assert hora in result.stdout
 
 
 @mark.parametrize('hora', ['12.50'])
 def test_converte_hora_cli_deve_conter_as_horas_com_dois_pontos(hora):
-    result = runner.invoke(app, '12:30')
+    result = runner.invoke(app, ['converte-hora', '12:30'])
+    assert hora in result.stdout
+
+
+def test_converte_hora_semanal_cli_deve_retornar_zero_ao_stdout():
+    result = runner.invoke(app, ['converte-hora-semanal'])
+    assert result.exit_code == 0
+
+
+def test_converte_hora_semanal_cli_deve_conter_as_horas_na_reposta():
+    hora = '18:00'
+    result = runner.invoke(app, ['converte-hora-semanal'])
     assert hora in result.stdout
