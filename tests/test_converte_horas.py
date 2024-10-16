c5986cdc1d7435bbe3746173a6c30c171550f57b
Gabriel Nadai <denadai123@gmail.com>
2024-10-16 18:10:22 -0300

    ajustes teste horas com ponto para 2 decimais e retirada comentarios

--- tests/test_converte_horas.py
+++ tests/test_converte_horas.py
@@ -1,32 +1,21 @@
-"""
-AAA - 3A - A3
-Arrange - Act - Assert!
-Arrumar - Agir -  Garantir!
-"""
 from pytest import mark, raises
 
 from conversor_horas_trabalho.converte_horas import converte_horas
 
 
 def test_converte_horas_deve_funcionar_com_horas_dois_pontos():
-    # Arrumar
     horas = '12:30'
 
-    # Agir
     result = converte_horas(horas)
 
-    # Garantir
     assert result
 
 
 def test_converte_horas_deve_funcionar_com_horas_um_ponto():
-    # Arrumar
     horas = '12.50'
 
-    # Agir
     result = converte_horas(horas)
 
-    # Garantir
     assert result
 
 
@@ -68,7 +57,7 @@ def test_converte_horas_deve_retornar_horas_convertidas_um_ponto_correto(
 
 @mark.parametrize(
     'horas,esperado',
-    [('12.10', '12:05'), ('12.20', '12:10'), ('12.99', '13:00')],
+    [('03.10', '03:05'), ('12.20', '12:10'), ('12.99', '13:00')],
 )
 def test_converte_horas_deve_retornar_horas_convertidas_dois_pontos_correto(
     horas, esperado
